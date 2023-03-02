b0c16806fff1cbc6eeb8ae7dea1fd1606874cf61
Juanfran <jfzamoraa@iessierradelasvillas.org>
2023-03-02 13:11:28 +0100

    Examen

--- Login.py
+++ Login.py
@@ -9,21 +9,26 @@ def main(page: ft.Page):
     #Fin --- Ejercicio 6
 
 
+    #Ejercicio 2
+    
+    
+    #Fin --- Ejercicio 2
+
+
     #Ejercicio 3
-    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    
+
     #Fin --- Ejercicio 3
 
     page.window_height=500
     page.window_width= 250
 
-    #Ejercicio 2
-    img = ft.Image(src="Logo.png",width=150,height=150)
-    #Fin --- Ejercicio 2
 
 
     tfnombre = ft.TextField(label="Nombre")
     #Ejercicio 4
-    tfpassword = ft.TextField(label="Contraseña")
+    
+
     #Fin --- Ejercicio 4
 
 
@@ -32,7 +37,7 @@ def main(page: ft.Page):
     #Fin-- Ejercicio 5
 
 
-    page.add(img,tfnombre,tfpassword)
+    page.add(img,tfnombre,tfpassword,btnEntrar)
     
 
 
